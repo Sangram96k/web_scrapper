4be4d01ece98717c4dd2c15d8cc959d632da251e
sangram_chavan_patil <100369989+Sangram96k@users.noreply.github.com>
2023-12-13 18:40:41 +0530

    Update web_scrapper.py

--- web_scrapper.py
+++ web_scrapper.py
@@ -1,28 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
 
-def scrape_quotes():
-    # URL of the website to scrape
-    url = ''
+def scrape_website(url):
+    try:
+        # Make a GET request to the specified URL
+        response = requests.get(url)
 
-    # Send a GET request to the URL
-    response = requests.get(url)
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Parse the HTML content of the page using BeautifulSoup
+            soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Parse the HTML content of the page using BeautifulSoup
-        soup = BeautifulSoup(response.text, 'html.parser')
+            # Find all the links on the page and print their titles
+            links = soup.find_all('a')
+            for link in links:
+                title = link.get('title', 'No Title')
+                print(f"Link Title: {title}")
 
-        # Extract data (quotes and authors in this case)
-        quotes = soup.find_all('span', class_='text')
-        authors = soup.find_all('small', class_='author')
+        else:
+            print(f"Error: Unable to fetch the page. Status code: {response.status_code}")
 
-        # Print the scraped data
-        for quote, author in zip(quotes, authors):
-            print(f"{quote.text.strip()} - {author.text.strip()}")
-
-    else:
-        print(f"Failed to retrieve the page. Status code: {response.status_code}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    scrape_quotes()
+    # Get the user input for the URL
+    url = input("Enter the URL to scrape: ")
+
+    # Call the scrape_website function with the user-provided URL
+    scrape_website(url)
